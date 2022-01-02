5c35b4aa988e615e05bbb0493a022d2e5b18c5a5
osama <osamaalaa133@gmail.com>
2022-01-02 11:38:29 +0400

    VirtualCareInitial

---
+++ components/sidebar.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+ <!--- Sidemenu -->
+ <div id="sidebar-menu">
+
+     <ul id="side-menu">
+
+         <li class="menu-title"> <a href="dashboard.php">VCS</a></li>
+         <li>
+             <ul class="nav flex-column">
+                 <li class="nav-item">
+                     <a class="nav-link" href="dashboard.php">Dashboard</a>
+                 </li>
+
+             </ul>
+         </li>
+         <li class="menu-title mt-2">Apps</li>
+
+         <li>
+             <a href="calendar.php">
+                 <i data-feather="calendar"></i>
+                 <span> Calendar </span>
+             </a>
+         </li>
+
+         <li>
+             <a href="https://virtualcaresolution.de/">
+                 <i data-feather="message-square"></i>
+                 <span> Chat </span>
+             </a>
+         </li>
+
+     </ul>
+
+ </div>
+ <!-- End Sidebar -->
