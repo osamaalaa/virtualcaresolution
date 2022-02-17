5d47b73880fb027e47d504e2d85407942d7853e9
vimspaul <vimspaul@gmail.com>
2022-02-17 10:27:09 +0400

    Vimala-Patients

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
      $servername = "localhost";  // 160.153.133.208
      $username = "root"; //"vcsdb";
-     $password = "ALPHa251611";//"Rehab@2021";//"Rehab@2021";
-     $dbname = "vcs";  //virtualcaresolution
+     $password = "";//"Rehab@2021";//"Rehab@2021";
+     $dbname = "virtualcaresolution";  //virtualcaresolution
     $conn=mysqli_connect($servername,$username,$password,"$dbname");
  
 ?>
